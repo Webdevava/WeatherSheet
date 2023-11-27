9f4f557f04db185f5b70eaabcf405026a640d757
Ankur <ankurauti@gmail.com>
2023-11-27 17:22:15 +0530

    updated

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,24 +4,25 @@ import axios from "axios";
 import "../styles/weather.scss";
 
 //images
-import temp from '../assets/thermometer.svg'
-import temp_min from '../assets/therm_loss.svg'
-import temp_max from '../assets/therm_add.svg'
-import wind from '../assets/wind.svg'
-import speed from '../assets/speed.svg'
-import direction from '../assets/direction.svg'
-import humidity from '../assets/humidity.svg'
-import pressure from '../assets/pressure.svg'
+import temp from "../assets/thermometer.svg";
+import temp_min from "../assets/therm_loss.svg";
+import temp_max from "../assets/therm_add.svg";
+import wind from "../assets/wind.svg";
+import speed from "../assets/speed.svg";
+import direction from "../assets/direction.svg";
+import humidity from "../assets/humidity.svg";
+import pressure from "../assets/pressure.svg";
 
 const Weather = ({ searchedLocation, currLocation, onSearch }) => {
   const [weatherData, setWeatherData] = useState(null);
   const apiKey = "d3d48be238ecb6ee1713db189f686066";
 
   useEffect(() => {
-
     axios
       .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${searchedLocation || currLocation}&appid=${apiKey}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?q=${
+          searchedLocation || currLocation
+        }&appid=${apiKey}&units=metric`
       )
       .then((response) => {
         setWeatherData(response.data);
@@ -30,10 +31,8 @@ const Weather = ({ searchedLocation, currLocation, onSearch }) => {
       .catch((error) => {
         console.error("Error fetching weather data:", error);
         onSearch(false); // Notify the parent about failed search
-      })
+      });
   }, [searchedLocation, currLocation, apiKey, onSearch]);
-
-
 
   if (searchedLocation || currLocation) {
     return (
@@ -45,15 +44,42 @@ const Weather = ({ searchedLocation, currLocation, onSearch }) => {
             ) : (
               <p className="myLocation">{currLocation}</p>
             )}
-            <p className="desc">{weatherData.weather[0].description}</p>
-            <p className="temp"> <img src={temp} alt="" /> {weatherData.main.temp}°C</p>
+            <p className="desc">
+              <img
+                className="weatherImg"
+                src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+                alt="weather-icon"
+              />
+              {weatherData.weather[0].description}
+            </p>
+            <p className="temp">
+              {" "}
+              <img src={temp} alt="" /> {weatherData.main.temp}°C
+            </p>
             <p className="feel">Feels like: {weatherData.main.feels_like}°C</p>
-            <p className="min">temp_min <img src={temp_min} alt="" />{weatherData.main.temp_min}°C</p>
-            <p className="max">temp_max <img src={temp_max} alt="" />{weatherData.main.temp_max}°C</p>
-            <p className="pressure">pressure<img src={pressure} alt="" /> {weatherData.main.pressure} hPa</p>
-            <p className="humid">humidity <img src={humidity} alt="" /> {weatherData.main.humidity}%</p>
-            <p className="wind_speed">wind_speed <img src={speed} alt="" /> {weatherData.wind.speed} m/s</p>
-            <p className="wind_dir">wind_direction <img src={direction} alt="" />{weatherData.wind.deg}°</p>
+            <p className="min">
+              temp_min <img src={temp_min} alt="" />
+              {weatherData.main.temp_min}°C
+            </p>
+            <p className="max">
+              temp_max <img src={temp_max} alt="" />
+              {weatherData.main.temp_max}°C
+            </p>
+            <p className="pressure">
+              pressure
+              <img src={pressure} alt="" /> {weatherData.main.pressure} hPa
+            </p>
+            <p className="humid">
+              humidity <img src={humidity} alt="" /> {weatherData.main.humidity}
+              %
+            </p>
+            <p className="wind_speed">
+              wind_speed <img src={speed} alt="" /> {weatherData.wind.speed} m/s
+            </p>
+            <p className="wind_dir">
+              wind_direction <img src={direction} alt="" />
+              {weatherData.wind.deg}°
+            </p>
             <p className="cloud">Cloudiness: {weatherData.clouds.all}%</p>
             <p className="sunrise">
               Sunrise:
@@ -63,13 +89,9 @@ const Weather = ({ searchedLocation, currLocation, onSearch }) => {
               Sunset:
               {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()}
             </p>
-            <p className="visible">Visibility: {weatherData.visibility / 1000} km</p>
-          
-            <img
-            className="weatherImg"
-              src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
-              alt="weather-icon"
-            />
+            <p className="visible">
+              Visibility: {weatherData.visibility / 1000} km
+            </p>
           </div>
         )}
       </div>
